945e0028aeab73acac0ea667c84814847c181ee1
codingwithcraig <your.email@example.com>
2026-05-09 01:02:41 +0200

    9 may edits: made a basic home page that i have to make mobile friendly and add more content to. also added a background image to the whole app. i have to make the background image more subtle and add a filter to it so that the text is more visible. i also have to add a navbar and a footer to the app. i will do that in the next commit.

--- portfolio/src/Pages/home.tsx
+++ portfolio/src/Pages/home.tsx
@@ -1,5 +1,11 @@
 
 import NavBar from "../components/navbar";
+import IGProfile from "../assets/Head.png";
+import '../PageStyles/home.css';
+import { IoIosMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io";
+import { FaGithub } from "react-icons/fa6";
 
 
 
@@ -8,8 +14,31 @@ export default function Home() {
   return (
     <div>
       <NavBar />
-      {/* <h1>Welcome to my Portfolio!</h1>
-      <p>This is where I will showcase my projects and skills.</p> */}
+
+      <div className="welcomeContainer">
+        <div className="welcomeC1">
+            <h2>HI THERE!</h2>
+            <h1>I'm Craig</h1>
+            <h3>Full-stack developer/ Junior-Developer</h3>
+
+        </div>
+
+        <div className="welcomeC2">
+            <img src={IGProfile} alt="IG profile"  className="Profile"/>
+
+            <div className="contactC">
+                {/* <h2>Contact Me</h2> */}
+                <p className="mail"><IoIosMail size={25} color="white"  /> <a href="mailto:clkhlongwani2@gmail.com" target="_blank" rel="noopener noreferrer">clkhlongwani2@gmail.com</a></p>
+                <p className="phone"><FaPhone size={25} color="white" /> <a href="tel:+27658328829" target="_blank" rel="noopener noreferrer">+27658328829</a></p>
+                <p className="linkedin"><IoLogoLinkedin size={24} color="white" /> <a href="https://www.linkedin.com/in/craig-hlongwani-447920359" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                <p className="github"><FaGithub size={22} color="white" /> <a href="https://github.com/CodingWithCraig" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+
+            </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
