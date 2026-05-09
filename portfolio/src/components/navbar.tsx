e39b1da33cea7e32d7a77e6a08589eafe44efac1
codingwithcraig <your.email@example.com>
2026-05-09 20:56:10 +0200

    a little nav issue solved

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -33,11 +33,11 @@ export default function NavBar(){
 
 
         <ul className="PortNav2">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#home" onClick={showNav}>Home</a></li>
+            <li><a href="#about" onClick={showNav}>About</a></li>
+            <li><a href="#skills" onClick={showNav}>Skills</a></li>
+            <li><a href="#projects" onClick={showNav}>Projects</a></li>
+            <li><a href="#contact" onClick={showNav}>Contact</a></li>
 
         </ul>
         </> 
