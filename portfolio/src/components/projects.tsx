0e39cb4b49a8d669947997f34fed242c187b21f6
codingwithcraig <your.email@example.com>
2026-05-10 16:17:56 +0200

    same

--- portfolio/src/components/projects.tsx
+++ portfolio/src/components/projects.tsx
@@ -35,11 +35,12 @@ const ProjectsSection: React.FC = () => {
     if (diff <= 0) return 'Launched!'; // Handle past dates
 
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((diff / (1000 * 60)) % 60);
-    const seconds = Math.floor((diff / 1000) % 60);
+    // const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    // const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    // const seconds = Math.floor((diff / 1000) % 60);
 
-    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    // return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    return `${days} days`;
   };
 
   // Sample projects data
